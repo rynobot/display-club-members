b77b24454e13ea7bded6566ecc7f12e166397ee7
Ryan Ott <rynobott@gmail.com>
2013-09-11 10:38:29 -0700

    Initial file information

--- display-club-members.php
+++ display-club-members.php
@@ -14,12 +14,13 @@
  * Description: Allows the display of users by role using shortcode.
  * Version:     1.0.0
  * Author:      Ryan Ott
- * Author URI:  TODO
- * Text Domain: plugin-name-locale
+ * Author URI:  http://ryanott.net
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- * Domain Path: /languages
- */
+  */
+
+/** Let's keep thing nice and neat...
+
 
 
 ?>
